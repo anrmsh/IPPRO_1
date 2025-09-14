96202f3dfaf653e5e652e637b96332ca2965bac3
Anna <annaryma2308@gmail.com>
2025-09-14 12:46:28 +0300

    Исправлена формула

---
+++ SumToN_6.java
@@ -0,0 +1,7 @@
+public class SumToN_6 {
+    public void calculateSum(int N){
+        int result;
+        result = (N+1)*N/2;
+        System.out.println("Сумма от 1 до " + N + " равна " + result);
+    }
+}
