ac3fb8d022dd1c2473ac7a797c8857314d9e2af8
Anna <annaryma2308@gmail.com>
2025-09-14 14:08:41 +0300

    Добавлено общее количество букв в тексте

--- VowelsCounter.java
+++ VowelsCounter.java
@@ -2,12 +2,13 @@ public class VowelsCounter {
     public void countVowels(String str){
         int count=0;
         String vowels = "AEIOUaeiou";
+        int totalCharacters = str.length();
         for (char c : str.toCharArray()) {
             if(vowels.indexOf(c)!=-1){
                 count++;
             }
         }
-        System.out.println("В строке " + count + " гласных");
+        System.out.println("В строке " + count + " гласных из " + totalCharacters + " символов");
 
     }
 }
