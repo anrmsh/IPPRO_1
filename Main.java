b52f93445032b1ed9488afe3dab915e000b626fa
Anna <annaryma2308@gmail.com>
2025-09-14 13:11:02 +0300

    Добавление проверки ввода числа

--- Main.java
+++ Main.java
@@ -8,5 +8,36 @@ public class Main {
         int N = scanner.nextInt();
         SumToN_6 obj = new SumToN_6();
         obj.calculateSum(N);
+
+
+        System.out.println("№7 Найти максимальное число из 3 чисел");
+        System.out.println("Введите a: ");
+        int a = scanner.nextInt();
+        System.out.println("Введите b: ");
+        int b = scanner.nextInt();
+        System.out.println("Введите c: ");
+        int c = scanner.nextInt();
+
+
+    }
+
+
+
+    public static int getIntInput(Scanner scanner, String prompt) {
+        int number = 0;
+        boolean validInput = false;
+
+        while (!validInput) {
+            System.out.print(prompt);
+            try {
+                number = scanner.nextInt();
+                validInput = true;
+            } catch (Exception e) {
+                System.out.println("Ошибка: пожалуйста, введите целое число.");
+                scanner.next(); 
+            }
+        }
+
+        return number;
     }
 }
