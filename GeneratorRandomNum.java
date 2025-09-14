5229d7673525915e2d361618207a793f803408b2
Anna <annaryma2308@gmail.com>
2025-09-14 13:26:09 +0300

    Добавлена сортировка массива рандомных чисел

--- GeneratorRandomNum.java
+++ GeneratorRandomNum.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class GeneratorRandomNum {
@@ -7,6 +8,12 @@ public class GeneratorRandomNum {
 
         for (int i=0; i<5; i++){
             nums[i] = random.nextInt(100)+1;
+            System.out.print(nums[i]+" ");
+        }
+
+        Arrays.sort(nums);
+        System.out.println("Отсортированный массив:");
+        for (int i=0; i<5; i++){
             System.out.print(nums[i]+" ");
         }
 
