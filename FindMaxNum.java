50783dbebc4af167c1253c21b113217427b68c58
Anna <annaryma2308@gmail.com>
2025-09-14 12:57:42 +0300

    Создан класс FindMaxNum

---
+++ FindMaxNum.java
@@ -0,0 +1,21 @@
+public class FindMaxNum {
+    public void findMaxNum(int a, int b, int c) {
+        System.out.println("Введенные числа:");
+        System.out.println("а = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        if (a==b && b==c){
+            System.out.println("Числа равны");
+        }
+        else if (a>b && a>c){
+            System.out.println("Максимальное число: " + a);
+        }
+        else if (b>a && b>c){
+            System.out.println("Максимальное число: " + b);
+        }
+        else {
+            System.out.println("Максимальное число: " + c);
+        }
+
+    }
+}
